7b1f5d773a1a89e8b88d351bbea236166d5bd602
Kirill Riazanov <riazanovkiril99@gmail.com>
2018-05-07 12:29:32 +0300

    Добавлена проверка на соотвествие открывающих и закрывающих скобок.

--- src/ru/rks/arithmeticPareser/ArithmeticParser.java
+++ src/ru/rks/arithmeticPareser/ArithmeticParser.java
@@ -1,7 +1,6 @@
 package ru.rks.arithmeticPareser;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
@@ -13,11 +12,10 @@ public class ArithmeticParser {
 
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
 
-            String str = bufferedReader.readLine();
-            checkForBrackets(str);
-            String expressionWithoutSpaces = deleteSpaces(str);
-            String[] strings = splitExpression(expressionWithoutSpaces);
-
+            String str;
+            System.out.println("Введите арифметическое выражение: ");
+            str = bufferedReader.readLine();
+            checkForValidate(str);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,20 +23,45 @@ public class ArithmeticParser {
     }
 
     /**
-     * Метод подсчитывает кол-во открывающих и закрывающих скобок в выражении и на основке
+     * Метод возвращает результат сопоставление полученного на вход арифметического выражения с регулярным.
      * @param string
+     * @return
      */
-    static void checkForValidate(String string) {
+    private static boolean checkForConformity(String string) {
 
-        if (string.matches("^(-?\\(*-?)*(\\d+(?:\\.\\d+)?|\\w+)+(\\s*\\)*\\s*" +
-                "[-+*^\\/]\\s*(-?\\(*-?)*(\\d+(?:\\.\\d+)?|\\w)+\\)*)+$")) {
-            System.out.println("Выражение введено правильно");
-        } else {
-            System.out.println("Выражение введено некорректно");
-        }
-
+        return string.matches("^(-?\\(*-?)*(\\d+(?:\\.\\d+)?|\\w+)+(\\s*\\)*\\s*" +
+                "[-+*^\\/]\\s*(-?\\(*-?)*(\\d+(?:\\.\\d+)?|\\w)+\\)*)+$");
     }
 
+    /**
+     * Метод выводит результаты проверки строки на соответствие регулярному выражению
+     * и соотвествие открывающих и закывающих скобок в консоль.
+     * @param string
+     */
+    static void checkForValidate(String string){
+
+        if(checkForConformity(string)){
+
+            if( checkForBrackets(string)) {
+
+                System.out.println("Выражение введено верно");
+
+            }
+
+                else {
+
+                    System.out.println("Несоотвествие открывающих и закрывающих скобок");
+
+                }
+
+            }
+
+            else{
+
+            System.out.println("Выражение введено некорректно");
+
+        }
+    }
     /**
      * Метод подсчитывает кол-во открывающих и закрывающих скобок в выражении и возвращает
      * булевое значение в зависимости от равенства их кол-ва.
@@ -71,17 +94,10 @@ public class ArithmeticParser {
     }
 
 
-
-    private static String deleteSpaces(String string) {
+    /*private static String deleteSpaces(String string) {
 
         return string.replaceAll(" ", "");
 
-    }
-
-    private static String[] splitExpression(String string) {
-
-        return string.split("[-+*/^)]");
-
-    }
+    }*/
 
 }
